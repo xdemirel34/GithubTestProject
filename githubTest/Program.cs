241d900405b71e285f192f53b9a70931b2526035
Mustafa Demirel <demirel34@windowslive.com>
2021-03-24 18:22:23 +0300

    Add project files.

---
+++ githubTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
